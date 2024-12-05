b9a340d977e97c86b796d7a4b82466488753c70b
lezgin123 <lezginyolal@hotmail.com>
2024-12-05 12:41:53 +0100

    boat repo

--- Første Eksamens projekt/BoatRepo.cs
+++ Første Eksamens projekt/BoatRepo.cs
@@ -1,31 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Dynamic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Første_Eksamens_projekt
 {
 	public class BoatRepo
 	{
-		public List<Boat> boats = new List<Boat>();
-		public void Add(Boat boats)
-		{
-			if (!boatList.Contains(boats)) boatList.Add(boats);
+		private List<Boat> boatList = new List<Boat>();
 
+		
+		public void Add(Boat boat)
+		{
+			if (!boatList.Contains(boat))
+			{
+				boatList.Add(boat);
+			}
 		}
 
-		public void Remove(Boat boat) 
-		
-		
+	
+		public Boat Remove(Boat boat)
 		{
-			Boat boat = Get(id);
-			if (boatList.Remove(boat))  
+			if (boatList.Remove(boat))
 			{
 				return boat;
 			}
 			return null!;
+		}
 
-		}jvjvjvdd
+	
+		public List<Boat> GetAll()
+		{
+			return new List<Boat>(boatList); 
+		}
+
+		
+		public Boat SearchBoat(string name)
+		{
+			foreach (Boat boat in boatList)
+			{
+				if (boat.Name == name) 
+				{
+					return boat;
+				}
+			}
+			return null!;
+		}
+
+		
+		public Boat UpdateEventRepo(Boat updatedBoat)
+		{
+			foreach (Boat existingBoat in boatList)
+			{
+				if (existingBoat.Id == updatedBoat.Id) 
+				{
+					
+					existingBoat.Name = updatedBoat.Name;
+					existingBoat.BoatModel = updatedBoat.BoatModel;
+					existingBoat.SailNumber = updatedBoat.SailNumber;
+					existingBoat.Measurements = updatedBoat.Measurements;
+					existingBoat.YearOfConstruction = updatedBoat.YearOfConstruction;
+					return existingBoat; 
+				}
+			}
+			return null!; 
+	}
+	}
 }
+
+
+
+
+
+
+
