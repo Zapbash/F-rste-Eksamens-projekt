1a073f31c71873e7aa961794b6fc31db68a4d692
Jannek Gam Nielsen <jan009@edu.zealand.dk>
2024-12-05 11:42:18 +0100

    EventRepo

--- Første Eksamens projekt/Event.cs
+++ Første Eksamens projekt/Event.cs
@@ -2,6 +2,25 @@
 {
     public class Event
     {
-        kfkfk 
+        public int Id { get; set; }
+        public string Location {  get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Event(int id, string location, string date, string time, string title, string description) 
+        {
+            Id = id;
+            Location = location;
+            Date = date;
+            Time = time;
+            Title = title;
+            Description = description;
+        }
+        public override string ToString()
+        {
+            return $"Event: {Id} {Location} {Date} {Time} {Title} {Description}";
+        }
     }
 }
