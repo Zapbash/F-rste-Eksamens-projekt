a8e1c01c4122c463f3377f26709182f730d4e414
lezgin123 <lezginyolal@hotmail.com>
2024-12-05 12:13:33 +0100

    hhh

--- Første Eksamens projekt/Boat.cs
+++ Første Eksamens projekt/Boat.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,39 @@ using System.Threading.Tasks;
 
 namespace Første_Eksamens_projekt
 {
-    internal class Boat
+    public class Boat
     {
-    }
+        public int Id { get; set; }
+		public string BoatType {get;set;}
+        public string Name {get;set;}
+        public string BoatModel {get;set;}
+
+        public int SailNumber { get;set;}
+        public double Measurements {get;set;}
+
+        public int YearOfConstruction {get;set;}
+        
+
+           public Boat(int id, string boatType, string name, string boatModel,int sailNumber, double measurements,int yearOfConstruction)
+		{ 
+            Id = id;
+            BoatType= boatType; 
+            
+            Name=name;
+            BoatModel = boatModel;
+            SailNumber = sailNumber;
+            Measurements = measurements;
+            YearOfConstruction = yearOfConstruction;
+
+
+
+		}
+
+		public override string ToString()
+		{
+			return $"{Id}{BoatType}{BoatModel}{Name}{SailNumber}{Measurements}{YearOfConstruction}";
+		}
+
+	}
+
 }
---
+++ Første Eksamens projekt/BoatRepo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Første_Eksamens_projekt
+{
+	public class BoatRepo
+	{
+		public List<Boat> boats = new List<Boat>();
+		public void Add(Boat boats)
+		{
+			if (!boatList.Contains(boats)) boatList.Add(boats);
+
+		}
+
+		public void Remove(Boat boat) 
+		
+		
+		{
+			Boat boat = Get(id);
+			if (boatList.Remove(boat))  
+			{
+				return boat;
+			}
+			return null!;
+
+		}
+}
